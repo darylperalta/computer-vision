b9f024f195cc445410757f25166bb2fde91d2f0b
Rowel Atienza <rowel@bitbucket.org>
2019-09-07 15:38:29 +0800

    MINE

--- detection/ssd/config.py
+++ detection/ssd/config.py
@@ -7,17 +7,16 @@ from __future__ import division
 from __future__ import print_function
 
 params = {
-        'dataset' : 'drinks',
-        'data_path' : 'dataset/drinks',
+        'dataset' : 'real_heads',
+        'data_path' : 'dataset/real_heads',
         'train_labels' : 'labels_train.csv',
         'test_labels' : 'labels_test.csv',
-        'epoch_offset': 0,
+        'epoch_offset': 30,
         'aspect_ratios': [1, 2, 0.5],
         'gt_label_iou_thresh' : 0.6,
-        'class_thresh' : 0.8,
+        'class_thresh' : 0.6,
         'iou_thresh' : 0.2,
         'is_soft_nms' : True,
-        'n_classes' : 3,
-        'classes' : ["background", "Summit", "Coke", "Pine Juice"],
-        'prices' : [0.0, 10.0, 40.0, 35.0]
+        'n_classes' : 1,
+        'classes' : ["background", "Head"],
         }
