43737d98ceb3432323bd2125d035c85e83e39372
Rowel Atienza <rowel.atienza@gmail.com>
2019-10-25 00:43:10 -0400

    meta learning on 1d Gaussian

--- meta-learning/maml.py
+++ meta-learning/maml.py
@@ -1,4 +1,4 @@
-'''Demonstrates MAML by learning to predict gaussian dist mean
+'''Demonstrates MAML by learning to predict a Gaussian dist mean
 
 python3 maml.py --n-samples=1000 --n-epochs=200 --batch-size=128
 
@@ -18,11 +18,11 @@ import argparse
 
 
 class SimpleMAML(nn.Module):
-    def __init__(self, args):
+    def __init__(self, args, device):
         super(SimpleMAML, self).__init__()
         self.args = args
-        self.n_tasks = self.args.n_tasks
-        hidden_units = 128
+        self.device = device
+        hidden_units = 256
         self.fc1 = nn.Linear(self.args.n_samples, hidden_units)
         self.fc2 = nn.Linear(hidden_units, hidden_units)
         self.fc3 = nn.Linear(hidden_units, 1)
@@ -30,15 +30,21 @@ class SimpleMAML(nn.Module):
         nn.init.kaiming_normal_(self.fc2.weight)
         nn.init.kaiming_normal_(self.fc3.weight)
         self.sample_means()
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.args.lr)
 
 
     def sample_means(self):
-        self.means = np.random.uniform(0.5, 1, self.args.n_tasks) #/ 100.
+        # n_tasks 1D Gaussians form distribution of tasks
+        # for meta-learning
+        # train means are from 0.5 to 1.0
+        self.means = np.random.uniform(0.5, 1, self.args.n_tasks)
+        # held out 1D Gaussian is for meta testing
+        # test mean is outside train means
         self.held_out = np.random.uniform(0.0, 0.2, 1)
 
 
     def forward(self, x):
+        # 3-layer MLP for mean prediction
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
@@ -46,15 +52,34 @@ class SimpleMAML(nn.Module):
         return x
 
 
+    def sample_input(self, mean, batch_size, n_samples):
+        samples = np.random.normal(mean, size=(batch_size, n_samples))
+        samples = np.reshape(samples, (batch_size, n_samples))
+        samples = torch.from_numpy(samples)
+        samples = samples.type(torch.FloatTensor)
+        samples = samples.to(self.device)
+        return samples
+
+    def sample_target(self, mean, batch_size):
+        mean = np.repeat(mean, batch_size)
+        mean = np.reshape(mean, (batch_size, -1))
+        mean = torch.from_numpy(mean)
+        mean = mean.type(torch.FloatTensor)
+        mean = mean.to(self.device)
+        return mean
+
     def train(self, test=False):
+        # number of task samples fr distribution of tasks
         n_task_samples = self.args.n_tasks // 2 
+        # number of data points as model input
         n_samples = self.args.n_samples
-        initial_values = []
+        # initial theta before update
+        theta = []
         losses = []
         scalar_losses = []
         mse = nn.MSELoss()
-        lr = 1e-3
         if test:
+            # 1-way, 1-shot meta-testing
             n_epochs = 1
             data = self.held_out
             indexes = [0]
@@ -67,93 +92,80 @@ class SimpleMAML(nn.Module):
         for epoch in range(n_epochs):
             self.params = list(self.parameters())
             device = self.params[0].device
-            initial_values = [p.clone().detach() for p in self.params]
+            # save the theta before computing phi
+            theta = [p.clone().detach() for p in self.params]
             total_loss = 0
-            final_values = []
+            phis = []
             if not test:
+                # during meta-training, sample fr distribution of tasks
                 indexes = np.random.randint(0, self.args.n_tasks, n_task_samples)
             for index in indexes:
-                for p, x in zip(self.params, initial_values):
+                # train per sampled task
+                for p, x in zip(self.params, theta):
                     p.data.copy_(x)
                 mean = data[index]
-                samples = np.random.normal(mean, size=(batch_size, n_samples))
-                samples = np.reshape(samples, (batch_size, n_samples))
-                samples = torch.from_numpy(samples)
-                samples = samples.type(torch.FloatTensor)
-                samples = samples.to(device)
+                samples = self.sample_input(mean, batch_size, n_samples)
                 y = self(samples)
-                mean = np.repeat(mean, batch_size)
-                mean = np.reshape(mean, (batch_size, -1))
-                mean = torch.from_numpy(mean)
-                mean = mean.type(torch.FloatTensor)
-                mean = mean.to(device)
+                mean = self.sample_target(mean, batch_size)
 
                 loss = mse(y, mean)
                 #losses.append(loss)
                 #scalar_losses.append(loss.item())
-                #updated = []
-                #grads = torch.autograd.grad(loss, self.params,  create_graph=True, retain_graph=True)
-                #for grad, param in zip(grads, self.params):
-                #    x = param - lr * grad
-                #    updated.append(x)
-                
-                #final_values.append(updated)
 
+                # compute phi
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
 
-                updated = [p.clone().detach() for p in list(self.parameters())]
-                final_values.append(updated)
+                # save phi (theta prime)
+                phi = [p.clone().detach() for p in list(self.parameters())]
+                phis.append(phi)
 
                 y = self(samples)
-                print(mean[0], y[0])
+                if not test:
+                    print(mean[0], y[0])
 
-
-            for updated, index in zip(final_values, indexes):
-                for p, x in zip(self.params, updated):
+            for phi, index in zip(phis, indexes):
+                for p, x in zip(self.params, phi):
                     p.data.copy_(x)
 
                 mean = data[index]
-                samples = np.random.normal(mean, size=(batch_size, n_samples))
-                samples = np.reshape(samples, (batch_size, n_samples))
-                samples = torch.from_numpy(samples)
-                samples = samples.type(torch.FloatTensor)
-                samples = samples.to(device)
+                samples = self.sample_input(mean, batch_size, n_samples)
                 y = self(samples)
-                mean = np.repeat(mean, batch_size)
-                mean = np.reshape(mean, (batch_size, -1))
-                mean = torch.from_numpy(mean)
-                mean = mean.type(torch.FloatTensor)
-                mean = mean.to(device)
-
+                mean = self.sample_target(mean, batch_size)
                 loss = mse(y, mean)
                 total_loss += loss
 
-            for p, x in zip(self.params, initial_values):
+            # apply gradient on theta
+            for p, x in zip(self.params, theta):
                 p.data.copy_(x)
             loss = total_loss / self.args.n_tasks
-            #print(loss)
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
+            # observe prediction after meta-testing
             if test:
                 y = self(samples)
-                print("Test: ", mean[0], y[0])
+                mean = mean[0].data.cpu()
+                y = y[0].data.cpu()
+                print("")
+                print("Meta-test:") 
+                print("Ground truth: %0.6f, Prediction: %0.6f: " % (mean, y))
+                print("")
+                print("Meta-train tasks: ", self.means)
 
 
+    # observe prediction before meta-testing
     def eval(self):
         with torch.no_grad():
             n_samples = self.args.n_samples
             mean = self.held_out
-            samples = np.random.normal(mean, size=(1, n_samples))
-            samples = np.random.normal(mean, size=(1, n_samples))
-            samples = np.reshape(samples, (1, n_samples))
-            samples = torch.from_numpy(samples)
-            samples = samples.type(torch.FloatTensor)
-            samples = samples.to(device)
+            samples = self.sample_input(mean, 1, n_samples)
             y = self(samples)
-            print("Pre-eval: ", mean, y)
+            y = y[0].data.cpu()
+            print("")
+            print("Without meta-testing: ")
+            print("Ground truth: %0.6f, Prediction: %0.6f: " % (mean, y))
         
 
 
@@ -161,25 +173,29 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='MAML on 1D Gaussian')
     parser.add_argument('--n-samples',
                         type=int,
-                        default=10,
-                        help='Number of samples per tasks')
+                        default=1000,
+                        help='Number of samples per task')
     parser.add_argument('--n-tasks',
                         type=int,
                         default=10,
-                        help='Number of tasks')
+                        help='Number of tasks (# of 1D Gaussians)')
     parser.add_argument('--n-epochs',
                         type=int,
-                        default=10,
+                        default=1000,
                         help='Number of epochs')
     parser.add_argument('--batch-size',
                         type=int,
                         default=128,
                         help='Batch size')
+    parser.add_argument('--lr',
+                        type=float,
+                        default=1e-3,
+                        help='Learning rate')
     args = parser.parse_args()
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
-    print(device)
-    simple_maml = SimpleMAML(args).to(device)
+    print("Training on ", device)
+    simple_maml = SimpleMAML(args, device).to(device)
     simple_maml.train()
     simple_maml.eval()
     simple_maml.train(test=True)
