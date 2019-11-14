1d2841cb98ab381e6ef53c87277cb973024a0635
Rowel Atienza <rowel.atienza@gmail.com>
2019-11-14 13:35:39 +0800

    fine tune

--- meta-learning/maml.py
+++ meta-learning/maml.py
@@ -22,7 +22,7 @@ class SimpleMAML(nn.Module):
         super(SimpleMAML, self).__init__()
         self.args = args
         self.device = device
-        hidden_units = 256
+        hidden_units = 128
         self.fc1 = nn.Linear(self.args.n_samples, hidden_units)
         self.fc2 = nn.Linear(hidden_units, hidden_units)
         self.fc3 = nn.Linear(hidden_units, 1)
@@ -32,6 +32,7 @@ class SimpleMAML(nn.Module):
         self.fc1.bias.data.fill_(0.01)
         self.fc2.bias.data.fill_(0.01)
         self.fc3.bias.data.fill_(0.01)
+        self.n_test = args.n_tests
         self.sample_means()
         self.meta_optim = torch.optim.Adam(self.parameters(), lr=self.args.meta_lr)
         self.update_optim = torch.optim.Adam(self.parameters(), lr=self.args.update_lr)
@@ -44,7 +45,7 @@ class SimpleMAML(nn.Module):
         self.means = np.random.uniform(0.5, 1, self.args.n_tasks)
         # held out 1D Gaussian is for meta testing
         # test mean is outside train means
-        self.held_out = np.random.uniform(0.0, 0.2, 1)
+        self.held_out = np.random.uniform(0.0, 0.2, self.n_test)
 
 
     def forward(self, x):
@@ -76,9 +77,9 @@ class SimpleMAML(nn.Module):
         return mean
 
 
-    def train(self, test=False):
+    def train(self, test=False, test_index=0):
         # number of task samples fr distribution of tasks
-        n_task_samples = self.args.n_tasks #// 2 
+        n_task_samples = self.args.n_tasks // 2 
         # number of data points as model input
         n_samples = self.args.n_samples
         # initial theta before update
@@ -87,7 +88,7 @@ class SimpleMAML(nn.Module):
         scalar_losses = []
         mse = nn.MSELoss()
         if test:
-            data = self.held_out
+            data = [self.held_out[test_index]]
             indexes = [0]
             #n_epochs = self.args.n_epochs
             n_epochs = 1
@@ -164,11 +165,19 @@ class SimpleMAML(nn.Module):
                 #print("Meta-train tasks: ", self.means)
 
 
+    def save_weights(self, path="model_maml.pt"):
+        torch.save(self.state_dict(), path)
+
+
+    def restore_weights(self,  path="model_maml.pt"):
+        self.load_state_dict(torch.load(path))
+
+
     # observe prediction before meta-testing
-    def eval(self):
+    def eval(self, test_index=0):
         with torch.no_grad():
             n_samples = self.args.n_samples
-            mean = self.held_out
+            mean = self.held_out[test_index]
             samples = self.sample_input(mean, 1, n_samples)
             y = self(samples)
             y = y[0].data.cpu()
@@ -198,23 +207,29 @@ if __name__ == '__main__':
                         help='Batch size (k-shot = batch_size)')
     parser.add_argument('--update-lr',
                         type=float,
-                        default=1e-3,
+                        default=5e-4,
                         help='update learning rate')
     parser.add_argument('--meta-lr',
                         type=float,
-                        default=5e-4,
+                        default=1e-4,
                         help='meta learning rate')
+    parser.add_argument('--n-tests',
+                        type=int,
+                        default=5,
+                        help='number of meta-test')
 
-    #torch.manual_seed(9007965514248702052)
+
     torch.manual_seed(104029687756708413)
-    seed = torch.seed()
     args = parser.parse_args()
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
     print("Training on ", device)
     simple_maml = SimpleMAML(args, device).to(device)
     simple_maml.train()
-    pre_eval = simple_maml.eval()
-    simple_maml.train(test=True)
-    print(pre_eval)
-    print(seed)
+    simple_maml.save_weights()
+    for i in range(simple_maml.n_test):
+        simple_maml.restore_weights()
+        pre_eval = simple_maml.eval(test_index=i)
+        simple_maml.train(test=True, test_index=i)
+        print(pre_eval)
+        print("--------------")
